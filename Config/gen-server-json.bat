c2dcece76a4c888d92dd243567dbc4dbde32662d
Roku.Wu <879463770@qq.com>
2025-04-02 11:18:37 +0800

    config

---
+++ Config/gen-server-json.bat
@@ -0,0 +1,3 @@
+dotnet.exe ./Tools/Luban.dll --target server --dataTarget json --codeTarget php-json --xargs outputDataDir=../Server/Json --xargs tableImporter.name=gameframex --xargs outputCodeDir=../Server/Config --conf ./Luban.conf
+
+pause
